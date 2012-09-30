898f391a27df2012f0cb312696bc740ba8fc4db8
Jeanne-Kamikaze <jeannekamikaze@gmail.com>
2012-09-30 15:55:54 +0200

    Renamed helper files

--- SFML/System.hs
+++ SFML/System.hs
@@ -1,21 +1,23 @@
 module SFML.System
 (
---    module SFML.System.Clock
+    module SFML.System.Clock
 --,   module SFML.System.InputStream
 --,   module SFML.System.Mutex
 --,   module SFML.System.Sleep
 --,   module SFML.System.Thread
-    module SFML.System.Vector2
---,   module SFML.System.Vector3
+,   module SFML.System.Time
+,   module SFML.System.Vector2
+,   module SFML.System.Vector3
 )
 where
 
 
---import SFML.System.Clock
+import SFML.System.Clock
 --import SFML.System.InputStream
 --import SFML.System.Mutex
 --import SFML.System.Sleep
 --import SFML.System.Thread
+import SFML.System.Time
 import SFML.System.Vector2
---import SFML.System.Vector3
+import SFML.System.Vector3
 
