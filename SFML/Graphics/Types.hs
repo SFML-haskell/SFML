2e8f0cbf7a15d12be9e130b43aaa7c3c2006040a
Jeanne-Kamikaze <jeannekamikaze@gmail.com>
2012-10-03 13:43:21 +0200

    Removed empty data decls

--- SFML/Graphics/Types.hs
+++ SFML/Graphics/Types.hs
@@ -1,20 +1,23 @@
-{-# LANGUAGE EmptyDataDecls #-}
 module SFML.Graphics.Types
 where
 
-data CircleShape
-data ConvexShape
-data Font
-data Image
-data Shader
-data RectangleSHape
-data RenderTexture
-data RenderWindow
-data Shape
-data Sprite
-data Text
-data Texture
-data Transformable
-data VertexArray
-data View
+
+import Foreign.Ptr (Ptr)
+
+
+newtype CircleShape = CircleShape (Ptr CircleShape)
+newtype ConvexShape = ConvexShape (Ptr ConvexShape)
+newtype Font = Font (Ptr Font)
+newtype Image = Image (Ptr Image)
+newtype Shader = Shader (Ptr Shader)
+newtype RectangleShape = RectangleShape (Ptr RectangleShape)
+newtype RenderTexture = RenderTexture (Ptr RenderTexture)
+newtype RenderWindow = RenderWindow (Ptr RenderWindow)
+newtype Shape = Shape (Ptr Shape)
+newtype Sprite = Sprite (Ptr Sprite)
+newtype Text = Text (Ptr Text)
+newtype Texture = Texture (Ptr Texture)
+newtype Transformable = Transformable (Ptr Transformable)
+newtype VertexArray = VertexArray (Ptr VertexArray)
+newtype View = View (Ptr View)
 
