f84b03be9a65df663fad324d2340b52104acd9c9
Jeanne-Kamikaze <jeannekamikaze@gmail.com>
2012-10-03 21:44:58 +0200

    Fixed export list

--- SFML/Graphics.hs
+++ SFML/Graphics.hs
@@ -1,12 +1,15 @@
 module SFML.Graphics
 (
-    module Graphics
+    module SFML.Graphics.Font
+,   module SFML.Graphics.Glyph
+,   module SFML.Graphics.Rect
+,   module SFML.Graphics.Types
 )
 where
 
 
-import SFML.Graphics.Font as Graphics
-import SFML.Graphics.Glyph as Graphics
-import SFML.Graphics.Rect as Graphics
-import SFML.Graphics.Types as Graphics
+import SFML.Graphics.Font
+import SFML.Graphics.Glyph
+import SFML.Graphics.Rect
+import SFML.Graphics.Types
 
