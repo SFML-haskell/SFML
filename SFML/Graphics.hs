49d29e24d47b97e26791abaf58a121204486881d
Jeanne-Kamikaze <jeannekamikaze@gmail.com>
2012-10-03 13:25:26 +0200

    SFML.Graphics.Types initial commit

--- SFML/Graphics.hs
+++ SFML/Graphics.hs
@@ -7,4 +7,5 @@ where
 
 import SFML.Graphics.Glyph as Graphics
 import SFML.Graphics.Rect as Graphics
+import SFML.Graphics.Types as Graphics
 
---
+++ SFML/Graphics/Types.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE EmptyDataDecls #-}
+module SFML.Graphics.Types
+where
+
+data CircleShape
+data ConvexShape
+data Font
+data Image
+data Shader
+data RectangleSHape
+data RenderTexture
+data RenderWindow
+data Shape
+data Sprite
+data Text
+data Texture
+data Transformable
+data VertexArray
+data View
+
