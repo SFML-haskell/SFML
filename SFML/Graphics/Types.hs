1cb65bdbdeccb2e8046ac1b230185f465f7b7868
Jeanne-Kamikaze <jeannekamikaze@gmail.com>
2012-10-05 12:43:07 +0200

    Added Storable instances for Texture and Shader

--- SFML/Graphics/Types.hs
+++ SFML/Graphics/Types.hs
@@ -2,7 +2,9 @@ module SFML.Graphics.Types
 where
 
 
-import Foreign.Ptr (Ptr)
+import Foreign.C.Types (CInt)
+import Foreign.Ptr
+import Foreign.Storable
 
 
 newtype CircleShape = CircleShape (Ptr CircleShape)
@@ -20,4 +22,20 @@ newtype Texture = Texture (Ptr Texture)
 newtype Transformable = Transformable (Ptr Transformable)
 newtype VertexArray = VertexArray (Ptr VertexArray)
 newtype View = View (Ptr View)
+
+
+instance Storable Texture where
+    sizeOf _ = sizeOf (undefined :: CInt)
+    alignment _ = alignment (undefined :: CInt)
+    
+    peek ptr = peek (castPtr ptr) >>= return . Texture
+    poke ptr (Texture p) = poke (castPtr ptr) p
+
+
+instance Storable Shader where
+    sizeOf _ = sizeOf (undefined :: CInt)
+    alignment _ = alignment (undefined :: CInt)
+    
+    peek ptr = peek (castPtr ptr) >>= return . Shader
+    poke ptr (Shader p) = poke (castPtr ptr) p
 
