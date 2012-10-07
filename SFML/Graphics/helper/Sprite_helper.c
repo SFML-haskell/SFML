0e256919937bf0a5c669644584c5467305658f24
Jeanne-Kamikaze <jeannekamikaze@gmail.com>
2012-10-07 23:08:51 +0200

    Added missing helpers

--- SFML/Graphics/helper/Sprite_helper.c
+++ SFML/Graphics/helper/Sprite_helper.c
@@ -60,3 +60,39 @@ void sfSprite_getInverseTransform_helper (const sfSprite* sprite, sfTransform* t
     *transform = sfSprite_getInverseTransform (sprite);
 }
 
+
+void sfSprite_setColor_helper (sfSprite* sprite, sfColor* color)
+{
+    sfSprite_setColor (sprite, *color);
+}
+
+
+void sfSprite_getColor_helper (const sfSprite* sprite, sfColor* color)
+{
+    *color = sfSprite_getColor (sprite);
+}
+
+
+void sfSprite_setTextureRect_helper (sfSprite* sprite, sfIntRect* rectangle)
+{
+    sfSprite_setTextureRect (sprite, *rectangle);
+}
+
+
+void sfSprite_getTextureRect_helper (const sfSprite* sprite, sfIntRect* rect)
+{
+    *rect = sfSprite_getTextureRect (sprite);
+}
+
+
+void sfSprite_getLocalBounds_helper (const sfSprite* sprite, sfFloatRect* rect)
+{
+    *rect = sfSprite_getLocalBounds (sprite);
+}
+
+
+void sfSprite_getGlobalBounds_helper (const sfSprite* sprite, sfFloatRect* rect)
+{
+    *rect = sfSprite_getGlobalBounds (sprite);
+}
+
