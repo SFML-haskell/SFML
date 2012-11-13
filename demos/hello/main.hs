028823e90d74c9a3dae5372b2913063104bce171
Jeanne-Kamikaze <jeannekamikaze@gmail.com>
2012-11-14 00:15:47 +0100

    Added sound and text

--- demos/hello/main.hs
+++ demos/hello/main.hs
@@ -1,3 +1,4 @@
+import SFML.Audio
 import SFML.Graphics
 import SFML.Window
 
@@ -9,20 +10,32 @@ main = do
     wnd <- createRenderWindow (VideoMode 640 480 32) "SFML Haskell Demo" [SFDefaultStyle] ctxSettings
     tex <- fmap fromJust $ textureFromFile "Haskell-Logo.png" Nothing
     spr <- fmap fromJust $ createSprite
+    fnt <- fmap fromJust $ fontFromFile "FreeSans.otf"
+    txt <- fmap fromJust $ createText
+    setTextString txt "Hello world!"
+    setTextFont txt fnt
+    setTextCharacterSize txt 50
+    setTextColor txt green
+    msc <- fmap fromJust $ musicFromFile "DST-BreakOut.ogg"
+    play msc
     setTexture spr tex True
-    loop wnd spr
+    loop wnd spr txt
+    destroyMusic msc
+    destroyText txt
+    destroyFont fnt
     destroySprite spr
     destroyTexture tex
     destroyRenderWindow wnd
 
 
-loop :: RenderWindow -> Sprite -> IO ()
-loop wnd spr = do
+loop :: RenderWindow -> Sprite -> Text -> IO ()
+loop wnd spr txt = do
     drawSprite wnd spr Nothing
+    drawText   wnd txt Nothing
     display wnd
     evt <- waitEvent wnd
     case evt of
         Nothing -> return ()
         Just SFEvtClosed -> return ()
-        _ -> loop wnd spr
+        _ -> loop wnd spr txt
 
