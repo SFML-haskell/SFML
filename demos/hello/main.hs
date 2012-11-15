890400da727201c9d5a46028a108ee1d6f5b2f8f
Jeanne-Kamikaze <jeannekamikaze@gmail.com>
2012-11-15 20:46:55 +0100

    Added demo resources to data-files

--- demos/hello/main.hs
+++ demos/hello/main.hs
@@ -3,20 +3,24 @@ import SFML.Graphics
 import SFML.Window
 
 import Data.Maybe (fromJust)
+import Paths_sfml_demos
 
 
 main = do
     let ctxSettings = Just $ ContextSettings 24 8 0 1 2
     wnd <- createRenderWindow (VideoMode 640 480 32) "SFML Haskell Demo" [SFDefaultStyle] ctxSettings
-    tex <- fmap fromJust $ textureFromFile "Haskell-Logo.png" Nothing
+    logoPath  <- getDataFileName "hello/Haskell-Logo.png"
+    fontPath  <- getDataFileName "hello/Vera.ttf"
+    musicPath <- getDataFileName "hello/DST-BreakOut.ogg"
+    tex <- fmap fromJust $ textureFromFile logoPath Nothing
     spr <- fmap fromJust $ createSprite
-    fnt <- fmap fromJust $ fontFromFile "Vera.ttf"
+    fnt <- fmap fromJust $ fontFromFile fontPath
     txt <- fmap fromJust $ createText
     setTextString txt "Hello world!"
     setTextFont txt fnt
     setTextCharacterSize txt 50
     setTextColor txt green
-    msc <- fmap fromJust $ musicFromFile "DST-BreakOut.ogg"
+    msc <- fmap fromJust $ musicFromFile musicPath
     play msc
     setTexture spr tex True
     loop wnd spr txt
